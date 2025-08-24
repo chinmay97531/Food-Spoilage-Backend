ba1ae304e8041de1bbca055c5e283ab4a2eee8f5
Chinmay <141347159+chinmay97531@users.noreply.github.com>
2025-08-24 22:35:48 +0530

    Update MongoDB URL to use placeholder

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const MONGODBURL = "mongodb+srv://ChinmayMittal:mBVwCN9LYdL6pIgz@cluster0.8mqmi.mongodb.net/FoodSpoilageDetection";
+export const MONGODBURL = "MONGO_DB_URL";
 export const JWT_SECRET = "@132FooD331Spoila21@geDetection!@#";
